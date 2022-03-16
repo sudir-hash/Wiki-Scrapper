701e7be5a8e038f1c7567566931292ac486c6908
Sudhir <81602162+sudir-hash@users.noreply.github.com>
2022-03-16 22:26:16 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,46 +1,46 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-''''
-work on fixing the class name
-'''
+
 
 xpath="/html/body/div[3]/div[3]/div[5]/div[1]/table[1]"
 
-info_table_class="infobox vcard"
-page_url='https://en.wikipedia.org/wiki/Canadian_Tire'
+# info_table_class="infobox vcard"
+page_url='https://en.wikipedia.org/wiki/Canada'
 
-def beauty_scarpe():
+
+
+def beauty_scarpe(page_url):
     page = requests.get(page_url).text
     soup = BeautifulSoup(page, 'html.parser')
 
 #    info_table_class="info vbox"
-    table = soup.find('table', class_=info_table_class)
-
+    # table = soup.find('table', class_=info_table_class)
+    table=soup.select("table[class*=info]") # with *= means: contains
     df = pd.read_html(str(table))
     df = pd.concat(df)
-    df = df.drop(index=0)
-    print(df)
+    return df
 
-# beauty_scarpe()
-def selenium_scrape():
-
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(page_url)
-    table = driver.find_elements_by_xpath (xpath)
-    table_contents=[]
-    # table = driver.find_elements(by=By.CLASS_NAME, value=info_table_class)
-    for el in table:
-        print(el.text)
-        table_contents.append(el.text)
+#beauty_scarpe()
+def selenium_scrape( link, xpath):
+	browser= webdriver.Chrome(ChromeDriverManager().install())
+	browser.get(link)
+	html = browser.find_element(By.XPATH, xpath)
+	return pd.read_html(html.get_attribute('outerHTML'))
 
 
-selenium_scrape()
+df=selenium_scrape(page_url,xpath)
+print(df)
+
+
+#using beautfiul soup
+# df=beauty_scarpe(page_url,xpath)
+# df.drop(index=0)
+# print(df)
 
 
 
